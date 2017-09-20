6e7ff20548318522d228083fb7bf429c4d0cb46b
Marko Djordjevic <makezi08@gmail.com>
2017-09-20 12:09:06 +1000

    Remove js files

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ var inProduction = (process.env.NODE_ENV === 'production');
 module.exports = {
   entry: {
     main: [
-      './src/js/main.js',
+      // './src/js/main.js',
       './src/scss/main.scss'
     ]
   },
